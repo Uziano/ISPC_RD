3da70368e4adb6d4d85b853ef0e1b09ea284e6b9
CandeMartt <candemartt@gmail.com>
2022-12-09 20:40:36 -0300

    Modificado el formulario del Login

--- FRONT/react-v2/src/components/Login.js
+++ FRONT/react-v2/src/components/Login.js
@@ -2,45 +2,48 @@ import React from 'react'
 
 function Login() {
   return (
-    <form>
-      <h3 className='display1'>Sign In</h3>
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-        />
-      </div>
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-        />
-      </div>
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
+  <div className="col-md-6 mx-auto">
+      <form>
+        <h3 className='display1 mb-3 text-left'>Sign In</h3>
+        <div className="mb-3">
+          <label>Correo</label>
           <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
+            type="email"
+            className="form-control"
+            placeholder="Correo"
           />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
         </div>
-      </div>
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
-      </p>
-    </form>
+        <div className="mb-3">
+          <label>Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Contraseña"
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Recuerdame
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Iniciar sesión
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Olvidaste tu <a href=" ">contraseña?</a>
+        </p>
+
+      </form>
+    </div>
   )
 }
 
