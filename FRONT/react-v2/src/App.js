9584d2d009e7b89171cef2550b8910f0f28f15e2
unknown <lucianoperaltadefazio@gmail.com>
2022-12-09 13:11:21 -0300

    Bootstrap configurado y funcionando

--- FRONT/react-v2/src/App.js
+++ FRONT/react-v2/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import CardB5 from './components/CardB5'
 
 function App() {
   return (
     <>
-    
+      {/* <CardB5 /> */}
       <Login/>
-    
     </>
   );
 }
---
+++ FRONT/react-v2/src/components/CardB5.js
@@ -0,0 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function CardB5() {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Card Subtitle
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
