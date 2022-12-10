efb2ae9b1c3cc4309d72f5c1b3f44c3f33d66978
CandeMartt <candemartt@gmail.com>
2022-12-09 22:28:17 -0300

    Se inicio el modelo usuario

--- BACK/usuario/models.py
+++ BACK/usuario/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    #Constructor
+    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+        user = self.model(
+            username = username,
+            email = email,
+            is_staff = is_staff,
+            is_superuser = is_superuser,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        return self._create_user(username, email, password, False, False, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        return self._create_user(username, email, password, True, True, **extra_fields)
+    
+class User(models.Model):
+    
+    #Atributos
+    mail = models.EmailField(
+        'Correo Electrónico',
+        max_length = 64, 
+        unique = True,)
+ 
+    username = models.CharField(
+        'Usuario',
+        max_length = 255,
+        unique = True)
+    
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+
