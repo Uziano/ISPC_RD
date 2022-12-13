345dd97fa0b75d2bdd191ef615155d4701f931db
unknown <lucianoperaltadefazio@gmail.com>
2022-12-13 00:06:29 -0300

    Modal nueva nota y card nota terminados

--- FRONT/react-v2/src/components/Notas/NotasList.js
+++ FRONT/react-v2/src/components/Notas/NotasList.js
@@ -1,11 +1,15 @@
 import NotasCard from '../Notas/NotasCard'
-
+// import {notas as data} from '../../data/'
 
 //Este es el que renderiza todas las notas con el .map()
 export default function NotasList() {
   return (
-    <>
-      <NotasCard />
-    </>
+    <div>
+      {/* <NotasCard title={data.title} description={data.description} /> */}
+      <NotasCard title={'Pasear al gatico'} description={'Tengo que pasear al gato que se portó re bien el condenado'} />
+      <NotasCard title={'Hello, Hello'} description={'Hello brian'} />
+      <NotasCard title={'Hello, Hello'} description={'Hello brian'} />
+      <NotasCard title={'Hello, Hello'} description={'Hello brian'} />
+    </div>
   )
 }
---
+++ FRONT/react-v2/src/data/notasMock.js
@@ -0,0 +1,17 @@
+export const notas = [
+    {
+        id: 0,
+        title: 'Mi primer nota',
+        description: 'Hello pepe como anda voce'
+    },
+    // {
+    //     id: 1,
+    //     title: 'Mi segundo nota',
+    //     description: 'Hello pepe como anda voce'
+    // },
+    // {
+    //     id: 2,
+    //     title: 'Mi tercer nota',
+    //     description: 'Hello pepe como anda voce'
+    // },
+]
