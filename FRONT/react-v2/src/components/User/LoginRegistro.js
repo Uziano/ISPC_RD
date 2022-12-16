b959a176210124be72f7a3d9bc9734d597e220eb
CandeMartt <candemartt@gmail.com>
2022-12-16 17:10:03 -0300

    Registro de usuarios

--- FRONT/react-v2/src/components/User/LoginRegistro.js
+++ FRONT/react-v2/src/components/User/LoginRegistro.js
@@ -1,24 +1,102 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import {useParams,useNavigate } from "react-router-dom";
+// import { useHistory, Navigate , useParams } from "react-router-dom";
 
-function LoginRegistro() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+// https://react-bootstrap.github.io/getting-started/introduction/
+
+// import { Alert } from 'react-bootstrap/Alert';
+
+// componentes
+import * as RegisterServer  from "../../server/RegisterServer";
+
+const User = () => {
+    // VARIABLES DEL COMPONENTE
+       const history = useNavigate();
+       const params = useParams();
+
+    // HOOK useState
+    const initialState = {    
+        id: 0,   
+        mail: '',
+        username:'',        
+        password:'',
+    };
+
+    const [user, setUser] = useState(initialState);
+
+    const handleInputChange = (e) => {
+        // console.log(e);
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+        setUser({ ...user,[e.target.name]: e.target.value});
+         console.log(user);
+        
+    }
+    
+    // ENVIO DEL FORMULARIO QUE LA DECLARAMOS EN EL onSubmit DEL FORM que enviara el usuario al servidor para grabarlo en la base de datos
+    // variable global de respuesta
+    var res;
+    const handleSubmit = async (e) => {
+        console.log(user);
+        console.log(" HANDLE SUBMIT:", user.id);
+        e.preventDefault();
+        // Podemos ver como se crea el nuevo usuario en un json
+        //  console.log(usuario);
+        try {
+          
+          if (!params.id) {
+            console.log(" ENTRA EN param.id NO EXISTE");
+            res = await RegisterServer.registerUser(user);
+            console.log("RES: ", res);
+            const data = await res.json();
+            console.log("Data: ",data);
+            if (data.id !== 0) {
+                console.log(" ENTRA EN data.id != 0");
+                setUser([]);
+            }
+          } else {
+            res = await RegisterServer.registerUser(params.id, user);
+          }
+           history("/");
+        } catch (error) {
+          console.log(error);
+        }
+    }
+
+    // EFECTO
+    useEffect(() => {
+        if (params.id) {
+          setUser(params.id);
+        }
+        // eslint-disable-next-line
+      }, []);
+
+    // RENDER o HTML o RETURN
+
   return (
   <div className="col-md-6 mx-auto p-4">
-      <form>
+      <form onSubmit= {handleSubmit}>
         <h3 className='display1 mb-3 text-left'>Sign Up</h3>
         <div className="mb-3">
           <label>Correo</label>
           <input
-            type="email"
+            type="text"
             className="form-control"
             placeholder="Correo"
+            value={user.mail}
+            onChange={handleInputChange}
           />
         </div>
         <div className="mb-3">
           <label>Usuario</label>
           <input
-            type="usuario"
+            type="text"
             className="form-control"
             placeholder="Usuario"
+            value={user.username}
+            onChange={handleInputChange}
           />
         </div>
         <div className="mb-3">
@@ -27,6 +105,9 @@ function LoginRegistro() {
             type="password"
             className="form-control"
             placeholder="Contraseña"
+            value={user.password}
+            onChange={handleInputChange}
+            aria-describedby="passwordHelpBlock"
           />
         </div>
         <div className="mb-3">
@@ -35,11 +116,10 @@ function LoginRegistro() {
             type="password"
             className="form-control"
             placeholder="Contraseña"
+            value={user.password}
+            onChange={handleInputChange}
+            aria-describedby="passwordHelpBlock"
           />
-        </div>
-        <div className="mb-3">
-                <label className="form-label">Agregar imagen</label>
-                <input type="file" className="form-control"/>
         </div>
         <div className="d-grid p-4">
           <button type="submit" className="btn btn-primary">
@@ -51,4 +131,4 @@ function LoginRegistro() {
   )
 }
 
-export default LoginRegistro
+export default User;
--- FRONT/react-v2/src/server/RegisterServer.js
+++ FRONT/react-v2/src/server/RegisterServer.js
@@ -1,32 +1,21 @@
 // VAMOS A CONECTARNOS A UNA API
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = 'http://127.0.0.1:8000/usuario-list/'
 
-// La solicitud es asincronico, es decir que tendra que esperar a que tenga una respuesta
-export const getHeroe = async() => {
-    return await fetch(API_URL);
-}
 
-export const traerHeroe = [
-    {
-        id:1,
-        nombre: 'BATMAN',
-        edad: 45,
-        universo: 2
-    }
-];
-
-export const updateHeroe = async() => {
-    return await fetch(API_URL);
-}
-
-export const listHeroe = async() => {
-    return await fetch(API_URL);
-}
-
-export const deleteHeroe = async() => {
-    return await fetch(API_URL);
-}
-
-export const createHeroe = async() => {
-    return await fetch(API_URL);
-}
+// Registrar un usuario
+export const registerUser = async (newUser) => {
+    console.log(newUser)
+    return await fetch(API_URL,{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        mode: 'no-cors', //
+        // headers: [],
+        body: JSON.stringify({
+            "mail": String(newUser.mail).trim(),
+            "username": String(newUser.username).trim(),
+            "password": String(newUser.password).trim()
+        })
+    });
+};
